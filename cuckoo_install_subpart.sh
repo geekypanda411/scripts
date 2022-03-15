83ef737b931e49e517df34004b6dd403c158c558
Aarjav Parashar <37975424+geekypanda411@users.noreply.github.com>
2022-03-16 03:37:10 +0530

    Added cuckoo installation script, required for part of the process.

---
+++ cuckoo_install_subpart.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+# NOTES: Run this script as: sudo -u <USERNAME> cuckoo-setup-virtualenv.sh
+
+# install virtualenv
+sudo apt-get update && sudo apt-get -y install virtualenv
+
+# install virtualenvwrapper
+sudo apt-get -y install virtualenvwrapper
+
+echo "source /usr/share/virtualenvwrapper/virtualenvwrapper.sh" >> ~/.bashrc
+
+# install pip for python3
+sudo apt-get -y install python3-pip
+
+# bash auto-complete for pip turned on
+pip3 completion --bash >> ~/.bashrc
+
+# To avoid installing with root
+pip3 install --user virtualenvwrapper
+
+echo "export VIRTUALENVWRAPPER_PYTHON=/usr/bin/python3" >> ~/.bashrc
+
+echo "source ~/.local/bin/virtualenvwrapper.sh" >> ~/.bashrc
+
+export WORKON_HOME=~/.virtualenvs
+
+echo "export WORKON_HOME=~/.virtualenvs" >> ~/.bashrc
+
+echo "export PIP_VIRTUALENV_BASE=~/.virtualenvs" >> ~/.bashrc 
